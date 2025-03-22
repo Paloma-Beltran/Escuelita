95425e2da442a3b4a719c3af60eebeb4ea3bbf67
Paloma-Beltran <paloma.beltran4396@alumnos.udg.mx>
2025-03-22 16:52:26 -0600

    correcciones

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    Route::resource('estudiantes', EstudianteController::class);
+    
 });
+Route::resource('estudiantes', EstudianteController::class);
